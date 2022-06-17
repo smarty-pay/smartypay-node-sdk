1afffdd51dd54b0fb9784b4eef391006246d33ab
edolganov <evgenij.dolganov@gmail.com>
2022-06-17 14:34:43 +0300

    Old api impl

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,20 @@
  * SMARTy Pay Node SDK
  * @author Evgeny Dolganov <evgenij.dolganov@gmail.com>
  */
-import {CreateInvoiceProps, CreateInvoiceReq, CreateInvoiceResp, InvoiceData, TokenType, InvoiceStatus} from './types';
+import {
+  SignReqProps,
+  CreateInvoiceReq,
+  CreateInvoiceResp,
+  InvoiceData,
+  TokenType,
+  InvoiceStatus,
+  CreatePushAddressReq, CreatePushAddressResp, TokensInfo
+} from './types';
 import {CryptoUtil} from './util/CryptoUtil';
 import {post} from './util/NetUtil';
 import {isString, removeEnd} from './util';
 
-export {CreateInvoiceReq, CreateInvoiceProps, InvoiceData, TokenType, InvoiceStatus}
+export {CreateInvoiceReq, SignReqProps, InvoiceData, TokenType, InvoiceStatus}
 
 
 export const SmartyPayAPI = {
@@ -18,15 +26,8 @@ export const SmartyPayAPI = {
    */
   async createInvoice(
     data: CreateInvoiceReq,
-    {
-      secretKey,
-      publicKey,
-      timeout,
-      host,
-    }: CreateInvoiceProps): Promise<InvoiceData> {
-
-    const now = Date.now();
-    const ts = Math.round(now / 1000).toString();
+    signProps: SignReqProps
+  ): Promise<InvoiceData> {
 
     const bodyData: any = {
       expiresAt: data.expiresAt.toISOString(),
@@ -37,25 +38,31 @@ export const SmartyPayAPI = {
       bodyData.metadata = isString(data.metadata)? data.metadata : JSON.stringify(data.metadata);
     }
 
-    const body = JSON.stringify(bodyData);
-    const messageToSign = ts + 'POST/integration/invoices' + body;
-    const sig = CryptoUtil.hmacSha256Hex(secretKey, messageToSign);
-    const targetHost = removeEnd(host || 'https://api.smartypay.io', '/');
-
-    const resp = await post(`${targetHost}/integration/invoices`, body, {
-      headers: {
-        'accept': 'application/json',
-        'content-type': 'application/json',
-        'x-api-key': publicKey,
-        'x-api-sig': sig,
-        'x-api-ts': ts,
-      },
-      timeout,
-    });
-
-    const {invoice} = JSON.parse(resp) as CreateInvoiceResp;
+    const {invoice} = await postSignReq<CreateInvoiceResp>(
+      '/integration/invoices',
+      bodyData,
+      signProps
+    );
 
     return invoice;
+  },
+
+  async createPushAddress(
+    data: CreatePushAddressReq,
+    signProps: SignReqProps
+  ): Promise<CreatePushAddressResp>{
+
+    const bodyData: any = {
+      cid: data.customerId,
+      token: TokensInfo[data.token].address,
+      blockchain: TokensInfo[data.token].blockchain,
+    }
+
+    return await postSignReq<CreatePushAddressResp>(
+      '/integration/push-addresses',
+      bodyData,
+      signProps
+    );
   },
 
   /**
@@ -74,4 +81,37 @@ export const SmartyPayAPI = {
     return SmartyPayAPI.getMessageSignature(message, secretKey) === signature;
   }
 
+}
+
+
+async function postSignReq<T>(
+  apiPath: string,
+  bodyData: any,
+  {
+    secretKey,
+    publicKey,
+    timeout,
+    host,
+  }: SignReqProps
+): Promise<T> {
+
+  const now = Date.now();
+  const ts = Math.round(now / 1000).toString();
+  const body = JSON.stringify(bodyData);
+  const messageToSign = ts + `POST${apiPath}` + body;
+  const sig = CryptoUtil.hmacSha256Hex(secretKey, messageToSign);
+  const targetHost = removeEnd(host || 'https://api.smartypay.io', '/');
+
+  const resp = await post(`${targetHost}${apiPath}`, body, {
+    headers: {
+      'accept': 'application/json',
+      'content-type': 'application/json',
+      'x-api-key': publicKey,
+      'x-api-sig': sig,
+      'x-api-ts': ts,
+    },
+    timeout,
+  });
+
+  return JSON.parse(resp) as T;
 }
--- src/types.ts
+++ src/types.ts
@@ -10,6 +10,11 @@ export interface CreateInvoiceReq {
   metadata?: string | Record<string, any>,
 }
 
+export interface CreatePushAddressReq {
+  customerId: string,
+  token: TokenType,
+}
+
 export type TokenType =
   // binance
   'bBUSD'
@@ -23,16 +28,24 @@ export type TokenType =
   | 'pmUSDT'
   ;
 
-export interface CreateInvoiceProps {
+export interface SignReqProps {
   publicKey: string,
   secretKey: string,
   timeout?: number,
   host?: string,
 }
 
+
+
 export interface CreateInvoiceResp {
   invoice: InvoiceData,
 }
+
+export interface CreatePushAddressResp {
+  address: string,
+  token: string,
+}
+
 
 export interface InvoiceData {
   id: string,
@@ -58,3 +71,53 @@ export type InvoiceStatus =
   | 'UnderPaid'
   | 'Expired'
   | 'Invalid';
+
+
+export type BlockhainType =
+  'BinanceMainNet'
+  | 'BinanceTestNet'
+  | 'PolygonMainNet'
+  | 'PolygonMumbaiNet'
+
+export interface TokenInfo {
+  address: string,
+  blockchain: BlockhainType
+}
+
+export const TokensInfo: Record<TokenType, TokenInfo> = {
+  bBUSD: {
+    blockchain: 'BinanceMainNet',
+    address: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  },
+  bUSDT: {
+    blockchain: 'BinanceMainNet',
+    address: '0x55d398326f99059fF775485246999027B3197955',
+  },
+  //
+  btBUSD: {
+    blockchain: 'BinanceTestNet',
+    address: '0xed24fc36d5ee211ea25a80239fb8c4cfd80f12ee',
+  },
+  btMNXe: {
+    blockchain: 'BinanceTestNet',
+    address: '0xd570e1ee81a8ca94008e1cf75c72b5e7a7b83bc5',
+  },
+  //
+  pUSDC: {
+    blockchain: 'PolygonMainNet',
+    address: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174',
+  },
+  pUSDT: {
+    blockchain: 'PolygonMainNet',
+    address: '0xc2132D05D31c914a87C6611C10748AEb04B58e8F',
+  },
+  //
+  pmUSDC: {
+    blockchain: 'PolygonMumbaiNet',
+    address: '0x7A7707cEE9bbF4D2Ce8f227865D456164841e4E5',
+  },
+  pmUSDT: {
+    blockchain: 'PolygonMumbaiNet',
+    address: '0x9b3273282f3b68dbF9b2c35f784cB1a012Cd670B',
+  },
+}
