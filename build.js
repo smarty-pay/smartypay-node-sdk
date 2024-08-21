ce7b7120fd391b35b5999e272e7a38ad11f1b0d6
EDolganov <evgenij.dolganov@gmail.com>
2024-08-21 14:00:54 +0300

    Libs update + prettier formating

--- build.js
+++ build.js
@@ -1,11 +1,10 @@
 const esbuild = require('esbuild');
-const { dtsPlugin } = require("esbuild-plugin-d.ts");
+const { dtsPlugin } = require('esbuild-plugin-d.ts');
 
 const libDest = 'dist/esbuild/smartypay-node-sdk.js';
 const cliDest = 'dist/esbuild/smartypay-cli.js';
 
-async function build(){
-
+async function build() {
   // lib
   await esbuild.build({
     logLevel: 'info',
@@ -15,11 +14,8 @@ async function build(){
     sourcemap: 'external',
     platform: 'node',
     outfile: libDest,
-    plugins: [
-      dtsPlugin()
-    ]
+    plugins: [dtsPlugin()],
   });
-
 
   // cli
   await esbuild.build({
@@ -31,12 +27,9 @@ async function build(){
     platform: 'node',
     outfile: cliDest,
   });
-
 }
-
 
 build().catch((e) => {
   console.error('build error', e);
-  process.exit(1)
+  process.exit(1);
 });
-
